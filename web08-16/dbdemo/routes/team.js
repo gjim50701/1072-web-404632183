3293c12e68790ef705d4431fe6a95acd132ca6e0
gjim50701 <gjim50701@kimo.com>
2019-06-18 18:33:40 +0800

    delete web01 dir

--- web08-16/dbdemo/routes/team.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const teamController = require('../controllers/team');
-
-router.get('/', teamController.getTeam);
-
-module.exports = router;
