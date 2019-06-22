24abf9f19dea937a727fa244def942b9087e1826
gjim50701 <gjim50701@kimo.com>
2019-06-22 17:52:15 +0800

    HTML/CSS/Bootstrap/MySQL

--- 7.TicTacToe/js/tictactoe.js
+++ 7.TicTacToe/js/tictactoe.js
@@ -2,17 +2,21 @@ $(document).ready(function () {
 
 
 
-    let turn = 0; //even for player 'o',odd for player 'x'
+    let turn = 1; //even for player 'o',odd for player 'x'
 
     $('#board li').on('click', function () {
         if ($(this).hasClass('disable')) {
             alert('Already filled');
-        } else if (turn % 2 == 0) {
+            turn--;
+        } else if (turn % 2 == 1) {
             $(this).text('o');
             $(this).addClass('disable o');
-        } else if (turn % 2 == 1) {
+        } else if (turn % 2 == 0) {
             $(this).text('x');
             $(this).addClass('disable x');
+        }
+        if(turn>4){
+        checkStatus(turn);
         }
         turn++;
     })
@@ -30,9 +34,38 @@ $(document).ready(function () {
 
     $('#reset').on('click', function () {
         $.fn.reset();
+        turn=1;
     })
 
-
-
-
 })
+
+function checkStatus(turn){
+
+    var winer;
+
+    if(turn % 2 == 1){
+        winer='player1'
+    }else{
+        winer='player2'
+    }
+
+    if($('#p1').text()==$('#p2').text()&&$('#p1').text()==$('#p3').text()&&$('#p1').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p1').text()==$('#p4').text()&&$('#p1').text()==$('#p7').text()&&$('#p1').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p1').text()==$('#p5').text()&&$('#p1').text()==$('#p9').text()&&$('#p1').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p2').text()==$('#p5').text()&&$('#p2').text()==$('#p8').text()&&$('#p2').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p3').text()==$('#p6').text()&&$('#p3').text()==$('#p9').text()&&$('#p3').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p3').text()==$('#p5').text()&&$('#p3').text()==$('#p7').text()&&$('#p3').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p4').text()==$('#p5').text()&&$('#p4').text()==$('#p6').text()&&$('#p4').text()!='+'){
+        alert('Winer is '+winer);
+    }else if($('#p7').text()==$('#p8').text()&&$('#p7').text()==$('#p9').text()&&$('#p7').text()!='+'){
+        alert('Winer is '+winer);
+    }else if(turn==9){
+        alert('Even');
+    }
+}
